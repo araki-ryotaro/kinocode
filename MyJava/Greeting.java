479c3d59743f0aa60f68f0f80ffea8beaabeb230
moppurin <98684224+moppurin@users.noreply.github.com>
2022-07-07 19:01:22 +0900

    [add]Java5 メソッド

---
+++ MyJava/Greeting.java
@@ -0,0 +1,11 @@
+// class (ファイル名)
+class Greeting{
+  public static void main(String[] args){
+    System.out.println("Good evening");
+
+    System.out.println("Good afternoon");
+
+    System.out.println("Good morning");
+  }
+
+}
