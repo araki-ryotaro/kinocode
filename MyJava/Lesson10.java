4d263ea3f4c7f63e0370d9a40dd7e48645796a16
moppurin <98684224+moppurin@users.noreply.github.com>
2022-07-12 19:16:06 +0900

    [add]Java10 条件分岐

---
+++ MyJava/Lesson10.java
@@ -0,0 +1,15 @@
+class Lesson10 {
+  public static void main(String[] args) {
+    // int age = 22;
+    // int age = 18;
+    int age = 0;
+
+    if (age >= 20) {
+      System.out.println("adult");
+    } else if (age==0) {
+      System.out.println("baby");
+    } else {
+      System.out.println("child");
+    }
+  }
+}
